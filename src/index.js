a7762a71b3fe851a871e350570938e32064f4432
Flint01686 <44868229+Flint01686@users.noreply.github.com>
2021-06-21 14:20:59 +0400

    webpack template

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import {Clock} from "./clock.js"
 import "../style/style.css"
-
-let output = document.getElementById("output");
-
-console.log("zalups");
-let timer = new Clock(output);
-timer.show(); 
-timer.start(); 
