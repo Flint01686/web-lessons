9409ffe2e37984a7b44ab24565f09e370ae3a06b
Flint01686 <44868229+Flint01686@users.noreply.github.com>
2021-06-21 17:20:19 +0400

    calc done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "../style/style.css"
+import {Calc} from "./calc"
+var field = document.getElementsByClassName("calc");
+var calc = new Calc(field);
+var buttons = document.getElementsByTagName("button");
+
+for (let i = 0; i< buttons.length-1;)
+    if (buttons[i].id !== "solver")
+        if (!buttons[i].onclick) buttons[i++].onclick = (e) => {
+            calc.addElem(e)
+        };
+console.log();
