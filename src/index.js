51ff11d16d0224e7110043a71d24dbd9bf8a8d66
Flint01686 <44868229+Flint01686@users.noreply.github.com>
2021-06-23 19:28:26 +0400

    todo done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "../style/style.css"
+let add = document.getElementById("add");
+let inp = document.getElementById("inp");
+let todo = document.getElementById("todo");
+
+function delItem(e)
+{
+    let item = e.currentTarget.parentNode
+    item.remove();
+}
+let listItemHtml = (text) => `
+        <input type="checkbox">
+        <span>${text}</span>
+        <button>Delete</button>
+        `
+function addList ()
+{
+    const newItem = document.createElement('li')
+    if (!inp.value) return;
+    newItem.innerHTML = listItemHtml(inp.value)
+    newItem.children[2].onclick = delItem;
+
+    todo.appendChild(newItem);
+}
+add.onclick = addList;
+inp.addEventListener('keydown', (e) => { if (e.code === "Enter") addList(); })
