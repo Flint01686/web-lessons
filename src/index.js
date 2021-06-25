296e05cab5d0292a1d4524f13f7e95ec31bb53cf
Flint01686 <44868229+Flint01686@users.noreply.github.com>
2021-06-25 14:55:48 +0400

    done

--- src/index.js
+++ src/index.js
@@ -2,24 +2,53 @@ import "../style/style.css"
 function quiz(title, annotation, questions, output)
 {
     let currentQuestionNum = 0;
-    let lastQuestion = null;
+    let lastQuestion = null
+    let sucess = []
     let htmlAnsByQuestArr = (question) => {
         let res=""
         let i = 0;
         for (let ans of question.answers)
-            res += `<label><input name="ans" type="${question.type}"  required
-            value="ansId${i}"> ${ans} </label>`
+            res += `<label><input name="ans" type="${question.type}"
+        value="${++i}"> ${ans} </label>`
         return res; 
     };
+
+    let addQuestionToForm = () => {
+        askTitle.textContent = questions[currentQuestionNum].text;
+        answersOut.innerHTML = htmlAnsByQuestArr(questions[currentQuestionNum])
+        lastQuestion = questions[currentQuestionNum]
+    }
+
+    function nextQuestion()
+    {
+        let currAnswers = Array.from(document.getElementsByName("ans")).filter(ans => ans.checked)
+        let currAnswersIds = [];
+        for (let currAns of currAnswers) currAnswersIds.push(currAns.value)
+        if (currAnswersIds.length === 0) {alert("Ты ответ то выбери, пожалуйста, ок да?"); return}
+        if (lastQuestion) 
+            if (JSON.stringify(lastQuestion.rightAnsIds) === JSON.stringify(currAnswersIds)) 
+                sucess.push(currAnswersIds)
+
+        if (questions.length-1 == currentQuestionNum) 
+        {
+            currentQuestionNum = 0;
+            alert(`${sucess.length} из ${questions.length} ${sucess.length === questions.length ? " красавчик уважаю." : " попытка - не пытка))"}`)
+            sucess = []
+            lastQuestion = null
+        }
+        else currentQuestionNum++
+        
+        addQuestionToForm()
+    }
 
     const html = `
     <strong>${title}</strong>
     <p>${annotation}</p>
-    <span>${questions[currentQuestionNum].text}</span>
+    <span id="text"></span>
     <form class="quiz__panel_ans" id="answers">
         
     </form>
-    <button oncklick>Next</button>
+    <button id="next">Next</button>
     `
 
     const quiz = document.createElement('div')
@@ -28,10 +57,15 @@ function quiz(title, annotation, questions, output)
     quiz.classList.add("quiz")
 
     output.appendChild(quiz)
+    let next = document.getElementById("next")
+    next.onclick = nextQuestion;
+
+    let askTitle = document.getElementById("text")
+    askTitle.textContent = questions[currentQuestionNum].text;
 
     let answersOut = document.getElementById("answers")
     console.log(answersOut);
-    answersOut.innerHTML = htmlAnsByQuestArr(questions[currentQuestionNum])
+    addQuestionToForm()
 }
 
 let title = "Who is this mysterious Colonel Sanders?";
@@ -41,8 +75,26 @@ let questions = [
         type: "radio", 
         text: "Когда родился Полковник Сандерс",
         answers: ["11 сентября 2001 года","9 сентября 1890", "19 сентября 2001 года", "10 сентября 1890"],
-        rightAnsId: 1
-    }
+        rightAnsIds: ["2"]
+    },
+    {
+        type: "radio", 
+        text: "Когда умер Полковник Сандерс",
+        answers: ["16 декабря 1980","9 сентября 1990", "25 января 1980", "21 июля 1982"],
+        rightAnsIds: ["1"]
+    },
+    {
+        type: "checkbox", 
+        text: "Почему Сандерс - полковник?",
+        answers: ["Служил во Вьетнаме", "Убирал дерьмо в конюшнях", "Служил на Кубе"],
+        rightAnsIds: ["2","3"]
+    },
+    {
+        type: "radio", 
+        text: "Что лежит у хобицца в карманце",
+        answers: ["Ничего"],
+        rightAnsIds: ["1"]
+    },
 ]
 let output = document.getElementById("root")
 quiz(title, annotation, questions, output)
