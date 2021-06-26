6f5ee1015910fa252e6a33282cc3c9dbbaaafced
Flint01686 <44868229+Flint01686@users.noreply.github.com>
2021-06-26 13:09:36 +0400

    add Intersection Observer api

--- src/index.js
+++ src/index.js
@@ -2,27 +2,27 @@ import "../style/style.css"
 
 let items = Array.from(document.querySelectorAll(".timeline li"))
 
-function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+const options = {
+    // родитель целевого элемента - область просмотра
+    root: null,
+    // без отступов
+    rootMargin: '0px',
+    // процент пересечения - половина изображения
+    threshold: 0.5
 }
 
-function check() {
-    for (var i = 0; i < items.length; i++) {
-        if (isElementInViewport(items[i])) {
-            items[i].classList.add("in-view");
-            items.splice(i--, 1);
+// создаем наблюдатель
+const observer = new IntersectionObserver((entries, observer) => {
+    // для каждой записи-целевого элемента
+    entries.forEach(entry => {
+        // если элемент является наблюдаемым
+        if (entry.isIntersecting) {
+            const item = entry.target
+            item.classList.add("in-view");
         }
-    }
-}
+    })
+}, options)
 
-// listen for events
-window.addEventListener("load", check);
-window.addEventListener("resize", check);
-window.addEventListener("scroll", check);
+// начинаю следиьт за всеми элементами таймлайна (P.S. годное API)
+for (let i = 0; i < items.length; i++) 
+    observer.observe(items[i]);
